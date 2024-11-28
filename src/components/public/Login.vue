530839d774f8b64f689adec75193e4ab50c5cde8
LuisVad <20213tn002@utez.edu.mx>
2024-11-28 13:02:24 -0600

    Cambio de Campos Login y Register
    Co-Authored-By: oscar foster utez <104937959+oscarfosteru@users.noreply.github.com>

--- src/components/public/Login.vue
+++ src/components/public/Login.vue
@@ -1,20 +1,38 @@
 <template>
   <div class="flex h-screen">
-    <div class="hidden lg:flex w-1/2 items-center justify-center bg-fdoscuro text-fdoscuro">
-
+    <div
+      class="hidden lg:flex w-1/2 items-center justify-center bg-fdoscuro text-fdoscuro"
+    >
       <div class="max-w-md text-center">
-        <img class="fixed left-[7%] top-[15%] logo" src="../../assets/images/logorealista.png" alt="" srcset=""/>
+        <img
+          class="fixed left-[7%] top-[15%] logo"
+          src="../../assets/images/LogoWaterhouse2.png"
+          alt="Logo"
+        />
         <section class="rotate left-[7%] top-[15%]">
           <svg viewBox="0 0 100 100" class="">
-            <path id="circlePath" fill="none" stroke-width="5" stroke="none" d="
+            <path
+              id="circlePath"
+              fill="none"
+              stroke-width="5"
+              stroke="none"
+              d="
           M 10, 50
           a 40,40 0 1,1 80,0
           a 40,40 0 1,1 -80,0
-        "/>
-            <text stroke="black" stroke-width="0.45" id="text" font-family="monospace" font-size="12"
-                  font-weight="bolder" fill="#e2e2e2">
+        "
+            />
+            <text
+              stroke="black"
+              stroke-width="0.45"
+              id="text"
+              font-family="monospace"
+              font-size="12"
+              font-weight="bolder"
+              fill="#e2e2e2"
+            >
               <textPath id="textPath" href="#circlePath">
-              🚚 Rápidos 📦 Precisos 📋 Seguros
+                🚚 Rápidos 📦 Precisos 📋 Seguros
               </textPath>
             </text>
           </svg>
@@ -23,51 +41,76 @@
     </div>
     <!-- Right Pane -->
     <div class="w-full bg-gray-200 lg:w-1/2 flex items-center justify-center">
-
       <div class="max-w-md w-full p-6">
         <h1 class="text-3xl font-bold mb-6 text-black text-center lg:hidden">
           Wharehouse Store
         </h1>
-        <h2 class="text-3xl font-semibold mb-6 text-black text-center sm:text-2xl lg:text-3xl">
+        <h2
+          class="text-3xl font-semibold mb-6 text-black text-center sm:text-2xl lg:text-3xl"
+        >
           Iniciar Sesión
         </h2>
         <h1 class="text-sm font-semibold mb-6 text-gray-500 text-center">
           Bienvenido a Wharehouse Store, Inicia Sesión para Continuar
         </h1>
-        <v-form class="space-y-4" @submit.prevent="onSubmit" ref="form" v-model="valid">
+        <v-form
+          class="space-y-4"
+          @submit.prevent="onSubmit"
+          ref="form"
+          v-model="valid"
+        >
           <div>
-            <v-text-field for="email" id="correo" name="correo" v-model="correo" :rules="emailRules"
-                          label="Correo electonico" required></v-text-field>
+            <v-text-field
+              for="email"
+              id="correo"
+              name="correo"
+              v-model="correo"
+              :rules="emailRules"
+              label="Correo Electrónico"
+              required
+            ></v-text-field>
           </div>
 
           <div>
-            <v-text-field v-model="contrasenia" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                          :rules="passwordRules" :type="show1 ? 'text' : 'password'" name="input-10-1"
-                          label="Contraseña" hint="La contraseña es requerida" counter
-                          @click:append="show1 = !show1"></v-text-field>
+            <v-text-field
+              v-model="contrasenia"
+              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="passwordRules"
+              :type="show1 ? 'text' : 'password'"
+              name="input-10-1"
+              label="Contraseña"
+              hint="La Contraseña es Requerida"
+              counter
+              @click:append="show1 = !show1"
+            ></v-text-field>
           </div>
           <div>
             <div class="mt-4 text-sm text-gray-600 text-center">
               <p>
-                ¿Olvidaste tu contraseña?
-                <a href="/restablecer/correo/" class="text-black hover:underline">Recuperala Aqui..!</a>
+                ¿Olvidaste tu Contraseña?
+                <a
+                  href="/restablecer/correo/"
+                  class="text-black hover:underline"
+                  >¡Recupérala Aquí!</a
+                >
               </p>
             </div>
             <v-btn
-                :loading="loading"
-                type="submit"
-                color="secondary"
-                class="w-full bg-fdoscuro text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300"
+              :loading="loading"
+              type="submit"
+              color="secondary"
+              class="w-full bg-fdoscuro text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300"
             >
               Iniciar Sesión
             </v-btn>
-            
           </div>
         </v-form>
         <div class="mt-4 text-sm text-gray-600 text-center">
           <p>
-            ¿Aun no eres parte de Foodster?
-            <a href="/home/registro/" class="text-black hover:underline">Registrate Aqui..!</a>
+            ¿Aun no eres parte de Wharehouse Store?
+            <a href="/home/registro/" class="text-black hover:underline"
+              >¡Registrate Aquí!</a
+            >
           </p>
         </div>
       </div>
@@ -76,22 +119,22 @@
 </template>
 
 <script>
-import {useAuthStore} from "@/stores";
+import { useAuthStore } from "@/stores";
 
 export default {
   data() {
     return {
       valid: true,
       loading: false,
-      correo: '',
-      contrasenia: '',
+      correo: "",
+      contrasenia: "",
       emailRules: [
-        v => !!v || 'El correo electronico es requerido',
-        (v) => /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(v) || "El correo electronico tiene un formato incorrecto",
+        (v) => !!v || "El correo electronico es requerido",
+        (v) =>
+          /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(v) ||
+          "El Correo Electrónico tiene un formato incorrecto",
       ],
-      passwordRules: [
-        v => !!v || 'la contraseña es requerida',
-      ],
+      passwordRules: [(v) => !!v || "la contraseña es requerida"],
       show1: false,
     };
   },
@@ -117,10 +160,11 @@ export default {
 }
 
 .logo {
-  height: 30rem;
-  width: 30rem;
+  margin-top: 7rem;
+  margin-left: 7.5rem;
+  height: 15rem;
+  width: 15rem;
 }
-
 
 @keyframes spin {
   0% {
--- src/components/public/Register.vue
+++ src/components/public/Register.vue
@@ -1,39 +1,38 @@
 <template>
   <div class="flex h-screen">
     <div
-        class="hidden lg:flex w-1/2 items-center justify-center bg-fdoscuro text-fdoscuro"
+      class="hidden lg:flex w-1/2 items-center justify-center bg-fdoscuro text-fdoscuro"
     >
       <div class="max-w-md text-center">
-        <img
-            class="fixed left-[7%] top-[15%] logo"
-            src="../../assets/images/logorealista.png"
-            alt=""
-            srcset=""
+               <img
+          class="fixed left-[7%] top-[15%] logo"
+          src="../../assets/images/LogoWaterhouse2.png"
+          alt="Logo"
         />
         <section class="rotate left-[7%] top-[15%]">
           <svg viewBox="0 0 100 100" class="">
             <path
-                id="circlePath"
-                fill="none"
-                stroke-width="5"
-                stroke="none"
-                d="
+              id="circlePath"
+              fill="none"
+              stroke-width="5"
+              stroke="none"
+              d="
             M 10, 50
             a 40,40 0 1,1 80,0
             a 40,40 0 1,1 -80,0
           "
             />
             <text
-                stroke="black"
-                stroke-width="0.45"
-                id="text"
-                font-family="monospace"
-                font-size="12.7"
-                font-weight="bolder"
-                fill="#e2e2e2"
+              stroke="black"
+              stroke-width="0.45"
+              id="text"
+              font-family="monospace"
+              font-size="12.7"
+              font-weight="bolder"
+              fill="#e2e2e2"
             >
               <textPath id="textPath" href="#circlePath">
-                 🚚 Rápidos 📦 Precisos 📋 Seguros
+                🚚 Rápidos 📦 Precisos 📋 Seguros
               </textPath>
             </text>
           </svg>
@@ -42,82 +41,88 @@
     </div>
     <!-- Right Pane -->
     <div class="w-full bg-gray-200 lg:w-1/2 flex items-center justify-center">
-
-      <div class="max-w-md w-full p-6" style="overflow-y: auto; max-height: calc(100vh - 4rem);">
-
+      <div
+        class="max-w-md w-full p-6"
+        style="overflow-y: auto; max-height: calc(100vh - 4rem)"
+      >
         <h1 class="text-3xl font-bold mb-6 text-black text-center lg:hidden">
           Wharehouse Store
         </h1>
         <h2
-            class="text-3xl font-semibold mb-6 text-black text-center sm:text-2xl lg:text-3xl"
+          class="text-3xl font-semibold mb-6 text-black text-center sm:text-2xl lg:text-3xl"
         >
           Registrate
         </h2>
         <h1 class="text-sm font-semibold mb-6 text-gray-500 text-center">
           Bienvenido a Wharehouse Store, Crea una Cuenta para Continuar
         </h1>
-        <v-form class="space-y-4" @submit.prevent="onSubmit" ref="form" v-model="valid">
+        <v-form
+          class="space-y-4"
+          @submit.prevent="onSubmit"
+          ref="form"
+          v-model="valid"
+        >
           <v-text-field
-              v-model="nombres"
-              :rules="nameRules"
-              :counter="30"
-              label="Nombre(s)"
-              required
+            v-model="nombreCompleto"
+            :rules="nameRules"
+            :counter="30"
+            label="Nombre Completo"
+            required
+          ></v-text-field>
+          <!--<v-text-field
+            v-model="primerApellido"
+            :rules="pApellidoRules"
+            :counter="30"
+            label="Apellido Paterno"
+            required
           ></v-text-field>
           <v-text-field
-              v-model="primerApellido"
-              :rules="pApellidoRules"
-              :counter="30"
-              label="Apellido Paterno"
-              required
+            v-model="segundoApellido"
+            :rules="sApellidoRules"
+            :counter="30"
+            label="Apellido Materno"
+            required
           ></v-text-field>
           <v-text-field
-              v-model="segundoApellido"
-              :rules="sApellidoRules"
-              :counter="30"
-              label="Apellido Materno"
-              required
-          ></v-text-field>
+            v-model="telefono"
+            :rules="telefonoRules"
+            :counter="10"
+            label="Numero de Teléfono"
+            required
+            type="number"
+          ></v-text-field>-->
           <v-text-field
-              v-model="telefono"
-              :rules="telefonoRules"
-              :counter="10"
-              label="Numero de Teléfono"
-              required
-              type="number"
-          ></v-text-field>
-          <v-text-field
-              for="email"
-              id="correo"
-              name="correo"
-              v-model="correo"
-              :rules="emailRules"
-              label="Correo Electrónico"
-              required
-              counter
+            for="email"
+            id="correo"
+            name="correo"
+            v-model="correo"
+            :rules="emailRules"
+            label="Correo Electrónico"
+            required
+            counter
           ></v-text-field>
 
           <v-text-field
-              v-model="contrasena"
-              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-              :rules="[...passwordRules, rules.required]"
-              :type="show1 ? 'text' : 'password'"
-              name="input-10-1"
-              label="Contraseña"
-              hint="La contraseña es requerida"
-              counter
-              @click:append="show1 = !show1"
+            v-model="contrasena"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[...passwordRules, rules.required]"
+            :type="show1 ? 'text' : 'password'"
+            name="input-10-1"
+            label="Contraseña"
+            hint="La contraseña es requerida"
+            counter
+            @click:append="show1 = !show1"
           ></v-text-field>
           <!--<div class="flex justify-center">
             <Captcha @solucion="ponerSolucion"/>
           </div>-->
           <div>
             <v-btn
-                :loading="loading"
-                :disabled="solucion === ''"
-                type="submit"
-                color="secondary"
-                class="w-full bg-fdoscuro text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300"
+              :loading="loading"
+              :disabled="solucion === ''"
+              type="submit"
+              color="secondary"
+              class="w-full bg-fdoscuro text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300"
             >
               Registrarse
             </v-btn>
@@ -127,23 +132,20 @@
           <p>
             ¿Ya eres parte de Foodster?
             <a href="/home/login/" class="text-black hover:underline"
-            >Incia sesion aqui...!</a
+              >¡Inicia Sesión Aquí!</a
             >
           </p>
         </div>
-
-
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import {useAuthStore} from "@/stores";
+import { useAuthStore } from "@/stores";
 import UsersServices from "@/services/UsersServices";
 
 export default {
-
   components: {
     Captcha: () => import("@/components/public/Captcha.vue"),
   },
@@ -152,10 +154,10 @@ export default {
     return {
       valid: true,
       correo: "",
-      nombres: '',
-      primerApellido: '',
-      segundoApellido: '',
-      telefono: '',
+      nombreCompleto: "",
+      //primerApellido: "",
+      //segundoApellido: "",
+      //telefono: "",
       show1: false,
       contrasena: "",
       solucion: "",
@@ -165,32 +167,40 @@ export default {
       },
       emailRules: [
         (v) => !!v || "El correo electronico es requerido",
-        (v) => /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(v) || "El correo electronico tiene un formato incorrecto",
+        (v) =>
+          /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(v) ||
+          "El correo electronico tiene un formato incorrecto",
       ],
       nameRules: [
-        v => !!v || 'Nombre requerido',
-        v => v.length <= 30 || 'El nombre debe de tener menos de 30 caracteres',
+        (v) => !!v || "Nombre requerido",
+        (v) =>
+          v.length <= 30 || "El nombre debe de tener menos de 30 caracteres",
       ],
-      pApellidoRules: [
-        v => !!v || 'Apellido Paterno requerido',
-        v => v.length <= 30 || 'El Apellido debe de tener menos de 30 caracteres',
+      /*pApellidoRules: [
+        (v) => !!v || "Apellido Paterno requerido",
+        (v) =>
+          v.length <= 30 || "El Apellido debe de tener menos de 30 caracteres",
       ],
       sApellidoRules: [
-        v => !!v || 'Apellido Materno requerido',
-        v => v.length <= 30 || 'El apellido debe de tener menos de 30 caracteres',
+        (v) => !!v || "Apellido Materno requerido",
+        (v) =>
+          v.length <= 30 || "El apellido debe de tener menos de 30 caracteres",
       ],
-      telefonoRules: [v => !!v || 'El telefono es requerido',
-        v => (v && v.length === 10) || 'El telefono debe tener 10 caracteres',
-        v => !isNaN(v) || 'Solo se permiten números'
-      ],
+      telefonoRules: [
+        (v) => !!v || "El telefono es requerido",
+        (v) => (v && v.length === 10) || "El telefono debe tener 10 caracteres",
+        (v) => !isNaN(v) || "Solo se permiten números",
+      ],*/
       passwordRules: [
-        v => !!v || 'la contraseña es requerida',
-        v => (v && v.length >= 8) || 'Minimo 8 caracteres',
-        v => (v && /[A-Z]/.test(v)) || 'Al menos una letra mayúscula',
-        v => (v && /[a-z]/.test(v)) || 'Al menos una letra minúscula',
-        v => /\d/.test(v) || 'Al menos un número',
-        v => (v && !/\s/.test(v)) || 'No espacios en blanco',
-        v => (v && /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>?]+/.test(v)) || 'Al menos un caracter especial',
+        (v) => !!v || "la contraseña es requerida",
+        (v) => (v && v.length >= 8) || "Minimo 8 caracteres",
+        (v) => (v && /[A-Z]/.test(v)) || "Al menos una letra mayúscula",
+        (v) => (v && /[a-z]/.test(v)) || "Al menos una letra minúscula",
+        (v) => /\d/.test(v) || "Al menos un número",
+        (v) => (v && !/\s/.test(v)) || "No espacios en blanco",
+        (v) =>
+          (v && /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>?]+/.test(v)) ||
+          "Al menos un caracter especial",
       ],
     };
   },
@@ -199,10 +209,10 @@ export default {
       if (this.$refs.form.validate()) {
         this.loading = true;
         let usuario = {
-          nombres: this.nombres,
-          primerApellido: this.primerApellido,
-          segundoApellido: this.segundoApellido,
-          telefono: this.telefono,
+          nombreCompleto: this.nombreCompleto,
+          //primerApellido: this.primerApellido,
+          //segundoApellido: this.segundoApellido,
+          //telefono: this.telefono,
           correo: this.correo,
           contrasena: this.contrasena,
           solucion: this.solucion,
@@ -210,19 +220,17 @@ export default {
         const response = await UsersServices.insertPublic(usuario);
         if (response) {
           await useAuthStore().login(this.correo, this.contrasena);
-          await this.$router.push('/home/perfil');
+          await this.$router.push("/home/perfil");
         }
         this.loading = false;
       }
-
     },
     ponerSolucion(solucion) {
       console.log(solucion);
       this.solucion = solucion;
-    }
+    },
   },
-  mounted() {
-  },
+  mounted() {},
 
   watch: {
     correo: function (val) {
@@ -241,8 +249,10 @@ export default {
 }
 
 .logo {
-  height: 30rem;
-  width: 30rem;
+  margin-top: 7rem;
+  margin-left: 7.5rem;
+  height: 15rem;
+  width: 15rem;
 }
 
 @keyframes spin {
